59b29fbd1a88ab8956f1d8fc0b6be4340d99410b
Julian Hartmann <julian.hartmann@students.ksba.ch>
2022-09-23 19:09:28 +0200

    solved the array problem

--- Scripts/index.js
+++ Scripts/index.js
@@ -11,7 +11,7 @@ startScreen();
 function startScreen() {
   $(".game-container").css("background-color", "#8093E8");
   $(".game-container").append(
-    "<div id='startScreen'><h1 class='gameText'>click to start</h1></div>"
+    "<div id='startScreen'><div><h1 class='gameText'>click to start</h1></div></div>"
   );
 
   $("#startScreen").mousedown(function () {
@@ -23,7 +23,7 @@ function waitForGreen() {
   $(".game-container").empty();
   $(".game-container").css("background-color", "#F44B49");
   $(".game-container").append(
-    "<div id='waitForGreen'><h1 class='gameText'>wait for green...</h1></div>"
+    "<div id='waitForGreen'><div><h1 class='gameText'>wait for green...</h1></div></div>"
   );
 
   cancelTurnGreen = setTimeout(turnGreen, 4500);
@@ -35,16 +35,16 @@ function waitForGreen() {
 }
 
 function clickedTooEarly() {
-    $(".game-container").empty();
-    $(".game-container").css("background-color", "#8093E8");
-    $(".game-container").append(
-        "<div id='clickedTooEarly'><div><h1 class='gameText'>too soon !</h1><h1 class='gameUnderText h5'>click to try again.</h1></div></div>"
-      );
+  $(".game-container").empty();
+  $(".game-container").css("background-color", "#8093E8");
+  $(".game-container").append(
+    "<div id='clickedTooEarly'><div><h1 class='gameText'>too soon !</h1><h1 class='gameUnderText h5'>click to try again.</h1></div></div>"
+  );
 
-      $("#clickedTooEarly").mousedown(function () {
-        waitForGreen();
-      });
-}   
+  $("#clickedTooEarly").mousedown(function () {
+    waitForGreen();
+  });
+}
 
 function turnGreen() {
   amountOfTries++;
@@ -52,33 +52,34 @@ function turnGreen() {
   $(".game-container").empty();
   $(".game-container").css("background-color", "#78F59A");
   $(".game-container").append(
-    "<div id='turnGreen'><h1 class='gameText'>CLICK</h1></div>"
+    "<div id='turnGreen'><div><h1 class='gameText'>CLICK</h1></div></div>"
   );
 
   countDownTimer = new Date();
 
-
   $("#turnGreen").mousedown(function () {
-    if(amountOfTries < 5) {
+    if (amountOfTries < 5) {
       onGreenClick();
-    }
-    else {
+    } else {
       resultScreen();
     }
   });
 }
 
-function onGreenClick() {
+function getResults() {
   result = new Date() - countDownTimer;
   results.push(result);
   console.log(results);
+}
+
+
+function onGreenClick() {
+  getResults()
 
   $(".game-container").empty();
   $(".game-container").css("background-color", "#8093E8");
   $(".game-container").append(
-    "<div id='onGreenClick'><h1 class='gameText'>" +
-    result +
-      " ms</h1></div>"
+    "<div id='onGreenClick'><div><h1 class='gameText'>" + result + " ms</h1><h1 class='gameUnderText h5'>click to keep going.</h1></div></div>"
   );
 
   $("#onGreenClick").mousedown(function () {
@@ -87,7 +88,9 @@ function onGreenClick() {
 }
 
 function resultScreen() {
-  jQuery.each(results, function(index, value) {
+  getResults()
+
+  jQuery.each(results, function (index, value) {
     total += value;
     count++;
   });
@@ -95,6 +98,8 @@ function resultScreen() {
   $(".game-container").empty();
   $(".game-container").css("background-color", "#8093E8");
   $(".game-container").append(
-    "<div id='turnGreen'><h1 class='gameText'>Your average was " + (total / count) + "</h1></div>"
+    "<div id='turnGreen'><div><h1 class='gameUnderText h5'>your average was</h1><h1 class='gameText'>" +
+      total / count +
+      " ms</h1><div class='line'></div><h1 class='gameUnderText h5'>click to retry again.</h1></div></div>"
   );
 }
