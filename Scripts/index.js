cb8fea2a4b677adba40a0652384bcd0dc407973e
Julian Hartmann <julian.hartmann@students.ksba.ch>
2022-09-23 18:31:12 +0200

    problem with getting the fifth try in an array

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,10 @@
 var countDownTimer = 0;
+var cancelTurnGreen; //cancelling the timeout for function 'turnGreen'
+var result;
+var results = [];
+var amountOfTries = 0;
+var total = 0;
+var count = 0;
 
 startScreen();
 
@@ -20,10 +26,29 @@ function waitForGreen() {
     "<div id='waitForGreen'><h1 class='gameText'>wait for green...</h1></div>"
   );
 
-  setTimeout(turnGreen, 4500);
+  cancelTurnGreen = setTimeout(turnGreen, 4500);
+
+  $("#waitForGreen").mousedown(function () {
+    clearTimeout(cancelTurnGreen);
+    clickedTooEarly();
+  });
 }
 
+function clickedTooEarly() {
+    $(".game-container").empty();
+    $(".game-container").css("background-color", "#8093E8");
+    $(".game-container").append(
+        "<div id='clickedTooEarly'><div><h1 class='gameText'>too soon !</h1><h1 class='gameUnderText h5'>click to try again.</h1></div></div>"
+      );
+
+      $("#clickedTooEarly").mousedown(function () {
+        waitForGreen();
+      });
+}   
+
 function turnGreen() {
+  amountOfTries++;
+
   $(".game-container").empty();
   $(".game-container").css("background-color", "#78F59A");
   $(".game-container").append(
@@ -34,16 +59,42 @@ function turnGreen() {
 
 
   $("#turnGreen").mousedown(function () {
-    onGreenClick();
+    if(amountOfTries < 5) {
+      onGreenClick();
+    }
+    else {
+      resultScreen();
+    }
   });
 }
 
 function onGreenClick() {
+  result = new Date() - countDownTimer;
+  results.push(result);
+  console.log(results);
+
   $(".game-container").empty();
   $(".game-container").css("background-color", "#8093E8");
   $(".game-container").append(
     "<div id='onGreenClick'><h1 class='gameText'>" +
-    (new Date() - countDownTimer) +
+    result +
       " ms</h1></div>"
+  );
+
+  $("#onGreenClick").mousedown(function () {
+    waitForGreen();
+  });
+}
+
+function resultScreen() {
+  jQuery.each(results, function(index, value) {
+    total += value;
+    count++;
+  });
+
+  $(".game-container").empty();
+  $(".game-container").css("background-color", "#8093E8");
+  $(".game-container").append(
+    "<div id='turnGreen'><h1 class='gameText'>Your average was " + (total / count) + "</h1></div>"
   );
 }
